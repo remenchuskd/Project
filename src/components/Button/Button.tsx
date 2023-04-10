9f1999bd5ecb2c1ab1ceed1c132effc72e6979b7
Abdulbarova Karina <Karina.Abdulbarova@vsevtyres.ru>
2023-04-10 20:24:46 +0300

    исправлен Button.tsx

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,26 +1,23 @@
-import styles from  "./Button.module.css";
+import styles from "./Button.module.css";
 import React from "react";
-export default function Button({color, outlined, text}) {
-    let buttonBorderClassName;
-    let buttonClassName;
-    switch(color){
-        case'blue':
-            buttonClassName=styles['Button_color_blue'];
-            break
-        case'green':
-            buttonClassName=styles['Button_color_green'];
-            break
-        case'darkblue':
-            buttonClassName=styles['Button_color_darkblue'];
-             break
-        
-    }
+export default function Button({ color, outlined, text }) {
+  let buttonClassName;
+  let buttonOutlinedClass = outlined ? styles.Button_outlined : "";
+  switch (color) {
+    case "blue":
+      buttonClassName = styles["Button_color_blue"];
+      break;
+    case "green":
+      buttonClassName = styles["Button_color_green"];
+      break;
+    case "darkblue":
+      buttonClassName = styles["Button_color_darkblue"];
+      break;
+  }
 
   return (
     <button
-      className={
-        outlined ? `${styles.Button} ${buttonClassName} ${styles.Button_outlined} ` : `${styles.Button} ${buttonClassName}`
-      }
+      className={`${styles.Button} ${buttonClassName} ${buttonOutlinedClass} `}
     >
       {text}
     </button>
